320d04a10ad976e1be13d3a6431939fd82c62c30
santa116-1 <totoplife116@gmail.com>
2025-01-14 18:25:32 +0000

    fix: user login

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -10,14 +10,14 @@ from app.api.deps import get_db
 router = APIRouter()
 
 @router.post("/login", response_model=Token)
-def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login_for_access_token(form_data: UserLogin, db: Session = Depends(get_db)):
 
-    user = db.query(User).filter(User.email == form_data.username).first()
+    user = db.query(User).filter(User.email == form_data.email).first()
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     access_token_expires = timedelta(minutes=30)
-    access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+    access_token = create_access_token(data={"sub": user.email, "id": user.id, "email": user.email}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/register", response_model=Token)
